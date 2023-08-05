e0fe17ace7bdc717d06f1c2d5e24fdc0ccf1ea3a
JohanHauteville <johan.hauteville@gmail.com>
2023-08-05 16:38:03 +0200

    Project initialized

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import Link from 'react-dom'
+
+function Header(){
+    return(
+        <header>
+            <img src="" alt=""></img>
+            <nav>
+                {/* <Link to="">Accueil</Link>
+                <Link to="">A Propos</Link> */}
+            </nav>
+        </header>
+    )
+}
+
+export default Header
