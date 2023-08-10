ad537faf958797806c88770a57560a4b9c2b3db9
JohanHauteville <johan.hauteville@gmail.com>
2023-08-10 11:28:24 +0200

    Page error redirection added

---
+++ src/components/Carrousel/index.jsx
@@ -0,0 +1,44 @@
+import './style.css'
+import { useState, useEffect } from 'react'
+
+
+function Carrousel({title,pictures}){
+    const [actualIndex, setActualIndex] = useState(0)
+    const [picture,setPicture] = useState(pictures[actualIndex])
+
+    useEffect(()=>{
+        setPicture(pictures[actualIndex])
+    },[actualIndex,pictures])
+
+    function nextPicture() {
+        setActualIndex(actualIndex + 1)
+        actualIndex>=(pictures.length-1) && setActualIndex(0)
+    }
+
+    function previousPicture() {
+        setActualIndex(actualIndex - 1)
+        actualIndex===0 && setActualIndex((pictures.length-1))
+    }
+  
+    return(
+        <div className='carrousel'>
+            <img src={picture} alt={title} className="logement__cover"/>
+            {pictures.length > 1 && 
+                <div>
+                    <div className='carrousel__buttons'>
+                        <i className="fa-solid fa-chevron-left carrousel__left-button" onClick={previousPicture}></i>
+                        <i className="fa-solid fa-chevron-right carrousel__right-button" onClick={nextPicture}></i>
+                    </div>
+                    <span className='carrousel__pagination'>
+                        {actualIndex+1}/{pictures.length}
+                    </span>
+                </div>
+            }
+       
+
+        </div>
+  
+    )
+}
+
+export default Carrousel
--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,42 +1,56 @@
 import './style.css'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import annonces from '../../assets/annonces.json'
 import Rating from '../../components/Rating'
 import HostInfo from '../../components/Host_info'
 import Collapse from '../../components/Collapse'
+import Carrousel from '../../components/Carrousel'
+import { useEffect } from 'react'
 
 function Logement(){
     const {id} = useParams()
     const logement = annonces.find(annonce=>annonce.id===id)
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        if(typeof(logement)==="undefined"){
+            navigate("/error")
+        }
+    }, [logement, navigate])
 
     return(
-        <main className='logement-page'>
-            <img src={logement.cover} alt={logement.title} className="logement__cover"/>
-            <section className='head__informations'>
-                <div>
-                    <h2>{logement.title}</h2>
-                    <p>{logement.location}</p>
-                    <section className='logement__tags'>
-                        {
-                            logement.tags.map(tag=>{
-                                return<div key={tag} className='logement__tags logement__tags--tag'>{tag}</div>
-                            })
-                        }
-                    </section>
-                </div>
-                <div className='host-and-rating'>
-                    <HostInfo name={logement.host.name} picture={logement.host.picture} />
-                    <Rating rate={logement.rating}/>
-                </div>
-            </section>
-            <section className='body-informations'>
-                <Collapse title="Description" content={logement.description} />
-                <Collapse title="Équipements" content={logement.equipments} />
-            </section>
-
-
-        </main>
-        
+        <>
+            {typeof(logement)==="undefined" ? 
+                ("")
+                :
+                (
+                    <main className='logement-page'>
+                        <Carrousel title={logement.title} pictures={logement.pictures}/>
+                        <section className='head__informations'>
+                            <div>
+                                <h2>{logement.title}</h2>
+                                <p>{logement.location}</p>
+                                <section className='logement__tags'>
+                                    {
+                                        logement.tags.map(tag=>{
+                                            return<div key={tag} className='logement__tags logement__tags--tag'>{tag}</div>
+                                        })
+                                    }
+                                </section>
+                            </div>
+                            <div className='host-and-rating'>
+                                <HostInfo name={logement.host.name} picture={logement.host.picture} />
+                                <Rating rate={logement.rating}/>
+                            </div>
+                        </section>
+                        <section className='body-informations'>
+                            <Collapse title="Description" content={logement.description} />
+                            <Collapse title="Équipements" content={logement.equipments} />
+                        </section>
+                    </main>
+                )
+            }
+        </>
     )
 }
 
