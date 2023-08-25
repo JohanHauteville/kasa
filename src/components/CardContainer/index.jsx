35f763105da159a4a65d2d19638ee931aa8de8be
JohanHauteville <johan.hauteville@gmail.com>
2023-08-25 17:24:25 +0200

    add cursor on components

--- src/components/CardContainer/index.jsx
+++ src/components/CardContainer/index.jsx
@@ -2,14 +2,15 @@ import './style.css'
 import Card from "../../components/Card"
 import annonces from "../../assets/annonces.json"
 
-function CardContainer(){
+function CardContainer() {
 
-    return(
+    return (
         <section className='card-section'>
             {/* Génère l'intégralité des cards des logements */}
-            {annonces.map( annonce => {
-                return <Card key={annonce.id} id={annonce.id} title = {annonce.title} picture={annonce.cover} />
-            })}
+            {annonces &&
+                annonces.map(annonce => {
+                    return <Card key={annonce.id} id={annonce.id} title={annonce.title} picture={annonce.cover} />
+                })}
         </section>
     )
 }
